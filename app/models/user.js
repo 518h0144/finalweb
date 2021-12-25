891236b3ca6355e6a4a353c5e02d657379f231a7
unknown <chuong.06022000@gmail.com>
2021-12-25 14:27:40 +0700

    test

--- app/models/user.js
+++ app/models/user.js
@@ -1,79 +1,66 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    min: 3,
-    max: 20,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    max: 50,
-    unique: true
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    min: 6,
-    default: "hello",
-  },
-  authType: {
-    type: String,
-    enum: ["google", "local"],
-    default: "local",
-  },
-  authGoogleID: {
-    type: String,
-  },
+const Schema = mongoose.Schema;
 
-  faculty: {
-    type: String,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      default: "hello",
+    },
+    authType: {
+      type: String,
+      enum: ["local", "google"],
+      default: "local",
+    },
+    authGoogleID: {
+      type: String,
+    },
 
-  class: {
-    type: String,
-  },
+    faculty: {
+      type: String,
+    },
 
-  avatar: {
-    type: String,
+    class: {
+      type: String,
+    },
+
+    avatar: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+    },
+    updateAt: {
+      type: Date,
+    },
+    role: {
+      type: String,
+      enum: ["student", "faculty", "admin"],
+      default: "student",
+    },
+    manageTopic: { type: Schema.Types.ObjectId, ref: "TypeInform" },
+    hookEnabled: {
+      type: Boolean,
+      default: false,
+    },
+    post: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   },
-  createdAt: {
-    type: Date,
-  },
-  updateAt: {
-    type: Date,
-  },
-  role: {
-    type: String,
-    enum: ["admin", "faculty", "student"],
-    default: "student",
-  },
-  // isAdmin: {
-  //   type: Boolean,
-  //   default:[],
-  // },
-  manageTopic: {
-    type: Schema.Types.ObjectId,
-    ref: "TypeInform"
-  },
-  hookEnabled: {
-    type: Boolean,
-    default: false,
-  },
-  post: [{
-    type: Schema.Types.ObjectId,
-    ref: "Post"
-  }],
-}, {
-  usePushEach: true,
-});
+  {
+    usePushEach: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   try {
